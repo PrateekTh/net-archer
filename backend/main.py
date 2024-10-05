ff9bda4ab0fe5c1f6e9a68892aed295c4e0a2c82
PrateekTh <prateek33thakur@gmail.com>
2024-10-05 20:35:41 +0530

    SSB - TF (small security bug - temporary fix)
    	modified:   .gitignore
    	modified:   backend/config/database.py
    	modified:   backend/main.py

--- backend/main.py
+++ backend/main.py
@@ -1,38 +1,6 @@
 from fastapi import FastAPI, Form, Request
-from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel
-from graph import checkDAG
+from fastapi.middleware.cors import CORSMiddlewareW
 from routes.route import router
-
-class Coordinates(BaseModel) : 
-    x: float
-    y: float
-    zoom: float  | None = None
-
-class Edge(BaseModel) : 
-    animated: bool
-    id: str
-    markerEnd: dict[str, str]
-    source: str
-    sourceHandle: str
-    target: str
-    targetHandle: str
-    type:str
-
-class Node(BaseModel) :
-    data: dict[str, str]
-    dragging: bool | None = None
-    height: float
-    position: Coordinates
-    positionAbsolute: Coordinates
-    selected: bool | None = None
-    type: str
-    width: float
-
-class Graph(BaseModel) :
-    edges: list[Edge]
-    nodes: list[Node]
-    viewport: Coordinates
 
 app = FastAPI()
 app.add_middleware(
