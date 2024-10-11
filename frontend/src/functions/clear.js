226ed4465a26ca1ef3c21a5631ae2fcc132402fa
PrateekTh <prateek33thakur@gmail.com>
2024-10-11 14:51:16 +0530

    Added Save Alerts

--- frontend/src/functions/clear.js
+++ frontend/src/functions/clear.js
@@ -2,6 +2,8 @@
 import { useReactFlow } from "reactflow";
 import { useStore } from "../store";
 import { useShallow } from 'zustand/shallow';
+// import { useState, useEffect } from 'react'
+// import { Alert } from "./alert";
 
 
 const selector = (state) => ({
@@ -14,23 +16,31 @@ const selector = (state) => ({
 
 export const ClearButton = () => {
 
+    
     const reactFlow = useReactFlow();
     const {
         setID,
         resetNodeIDs,
         resetEdges,
     } = useStore( useShallow(selector));
+    // const [alert, setAlert] = useState(null);
+
+    // useEffect(() => {
+    //     setTimeout(()=> setAlert(null), 6000)
+    // }, [alert]);
 
     const handleClear = () => {
         resetEdges();
         reactFlow.setNodes([]);
         resetNodeIDs();
+        // setAlert("Graph cleared!")
         // setID(null);
     }
 
     return (<>
         <div>
             <button type="submit" style={{background: "rgb(91, 103, 194)"}} onClick={handleClear}> Clear </button>
+            {/*alert && Alert({type: 'text', message: alert})*/}
         </div>
     </>);
 }
--- frontend/src/functions/save.js
+++ frontend/src/functions/save.js
@@ -2,31 +2,41 @@
 import { useReactFlow } from "reactflow";
 import { useStore } from "../store";
 import { setGraph } from "../modules/connection";
+import { useState, useEffect } from 'react';
+import { Alert } from "./alert";
+
 
 export const SaveButton = () => {
     const reactFlow = useReactFlow();
     const id = useStore((state) => state.id);
     const setID = useStore((state) => state.setID);
+    const [alert, setAlert] = useState(null);
+
+    useEffect(() => {
+        setTimeout(()=> setAlert(null), 6000)
+    }, [alert]);
 
     const handleSave = async () => {
         const graph = reactFlow.toObject();
         let new_id = null;
-        console.log(id);
+        console.log(graph);
         if(id){
-            console.log("exists");
+            // console.log("exists");
             new_id = await setGraph(graph, id);
 
         }else{
             new_id = await setGraph(graph);
         }
 
-        console.log(new_id);
+        // console.log(new_id);
+        setAlert("Saved to Database!")
         setID(new_id);
     }
 
     return (<>
             <div>
                 <button type="submit" onClick={handleSave}> Save </button>
+                {alert && Alert({type: 'text', message: alert})}
             </div>
         </>
     );
