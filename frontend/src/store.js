8d4a425f66036db6d665d23cf000b13ee08c56c6
PrateekTh <prateek33thakur@gmail.com>
2024-10-14 14:58:52 +0530

    Removed default offline flow

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -13,87 +13,12 @@ console.log("Creating Store")
 
 // flow - an offline default starting graph when the backend server is down (decent stuff)
 
-let flow = {
-    "nodes": [
-        {
-            "width": 323,
-            "height": 133,
-            "id": "server-def-1",
-            "type": "server",
-            "position": {
-                "x": 553,
-                "y": 209
-            },
-            "data": {
-                "id": "server-def-1",
-                "nodeType": "server",
-                "tag": ""
-            },
-            "selected": true,
-            "positionAbsolute": {
-                "x": 553,
-                "y": 209
-            },
-            "dragging": false
-        },
-        {
-            "width": 200,
-            "height": 141,
-            "id": "client-def-1",
-            "type": "client",
-            "position": {
-                "x": 184,
-                "y": 153
-            },
-            "data": {
-                "id": "client-def-1",
-                "nodeType": "client",
-                "tag": ""
-            },
-            "selected": false,
-            "dragging": false,
-            "positionAbsolute": {
-                "x": 184,
-                "y": 153
-            }
-        }
-    ],
-
-    "edges": [
-        {
-            "source": "client-def-1",
-            "sourceHandle": "client-def-1-output",
-            "target": "server-def-1",
-            "targetHandle": "server-def-1-system",
-            "type": "custom",
-            "markerEnd": {
-                "type": "arrowclosed",
-                "strokeWidth": 4
-            },
-            "markerStart": {
-                "type": "arrowclosed",
-                "strokeWidth": 4
-            },
-            "data": {
-                "label": "...",
-                "anim": true,
-                "bi": false
-            },
-            "id": "reactflow__edge-client-def-1client-def-1-output-server-def-1server-def-1-system",
-            "selected": false
-        }
-    ],
-    "viewport": {
-        "x": 0,
-        "y": 0,
-        "zoom": 1
-    }
-};
+let flow = null
 
 export const useStore = create((set, get) => ({
-    id: flow.id || null,
-    nodes: flow.nodes || [],
-    edges: flow.edges || [],
+    id: flow?.id || null,
+    nodes: flow?.nodes || [],
+    edges: flow?.edges || [],
 
     setID: (new_id) => set((state) => ({ id: new_id })),
 
